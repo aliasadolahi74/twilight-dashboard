e2317f4a5bfae42e6220cb297a6f2b4c3a3ce827
Ali Asadollahi <ali.asadolahi74@gmail.com>
2025-02-14 18:27:16 +0000

    Fix sidebar active items animation

--- src/features/Dashboard/DashboardMenu/MenuItem/index.tsx
+++ src/features/Dashboard/DashboardMenu/MenuItem/index.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { ChevronRight } from "lucide-react";
 import { cn } from "shadcn/lib/utils";
 import { DashboardMenuContext } from "@/src/features/Dashboard/DashboardMenu";
+import SubmenuItem from "@/src/features/Dashboard/DashboardMenu/SubmenuItem";
 
 type IMenuItemProps = {
   properties: IDashboardItemType;
@@ -21,19 +22,32 @@ const MenuItem: React.FC<IMenuItemProps> = ({ properties }) => {
   };
 
   return (
-    <button onClick={handleMenuItemClick} className='flex flex-row items-center justify-between h-[42px]'>
-      <div
-        className={cn("flex flex-row items-center gap-1.5 text-neutral-400", {
-          "!text-primary": isActive,
-        })}
-      >
-        <span>{icon}</span>
-        <span className='text-14'>{title}</span>
-      </div>
+    <div className='flex group flex-col w-full'>
+      <button onClick={handleMenuItemClick} className='flex flex-row items-center justify-between h-[42px]'>
+        <div
+          className={cn("flex flex-row items-center gap-1.5 text-neutral-400", {
+            "!text-primary": isActive,
+          })}
+        >
+          <span>{icon}</span>
+          <span className='text-14'>{title}</span>
+        </div>
+        {submenu && submenu.length > 0 && (
+          <ChevronRight className={cn("transition-all duration-300 size-[12px]", { "rotate-90": isActive })} />
+        )}
+      </button>
       {submenu && submenu.length > 0 && (
-        <ChevronRight className={cn("transition-all duration-300 size-[12px]", { "rotate-90": isActive })} />
+        <div className={cn("grid grid-rows-[0fr] transition-all duration-300", { "grid-rows-[1fr]": isActive })}>
+          <div className='overflow-hidden'>
+            <div className='flex flex-col gap-1'>
+              {submenu.map((submenuItem) => (
+                <SubmenuItem properties={submenuItem} key={submenuItem.slug} />
+              ))}
+            </div>
+          </div>
+        </div>
       )}
-    </button>
+    </div>
   );
 };
 
